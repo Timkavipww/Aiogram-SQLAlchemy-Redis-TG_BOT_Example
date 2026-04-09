8679ca2c95f6f7fc52c049d7a45cb58403396114
timka <easy.timurwin@gmail.com>
2026-04-09 20:28:28 +0300

    fix ensure user

--- bot/app/middleware/ensure_user_middleware.py
+++ bot/app/middleware/ensure_user_middleware.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, Callable
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Message, CallbackQuery
+from aiogram.types import TelegramObject, Message, CallbackQuery, Update
 from app.services.user_service import UserService
 from app.config import logger
 
@@ -8,16 +8,18 @@ class EnsureUserMiddleware(BaseMiddleware):
     async def __call__(self, handler, event: Update, data):
         if event.message:
             session = data.get("session")
+            cache = data.get("cache")
+
             if session is None:
                 logger.warning("[EnsureUserMiddleware] Нет session в данных")
                 return await handler(event, data)
 
-            service = UserService(session)
+            service: UserService = UserService(session, cache=cache)
             user_id = event.message.from_user.id
             username = event.message.from_user.username
-            user = await service.get_by_id(user_id)
+            user = await service.get_user(user_id)
             if not user:
-                await service.create(id=user_id, username=username)
+                await service.create_user(user_id=user_id, username=username)
                 logger.info(f"[EnsureUserMiddleware] Создан новый пользователь: {user_id}")
 
         return await handler(event, data)
